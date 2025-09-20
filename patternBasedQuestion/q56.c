f9ee1f45f46dcb50caaec9d8ac4b0bb011dc68a8
Amrit-raj50 <amrit.raj.cg@gmail.com>
2025-09-20 18:59:54 +0530

    Final Touch

--- patternBasedQuestion/q56.c
+++ patternBasedQuestion/q56.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-    int n= 7;
+    int n= 9;
     for(int i = 1 ; i <= n ; i++)
     {
         for(int j = 1 ; j <= n ; j++)
--- patternBasedQuestion/q57.c
+++ patternBasedQuestion/q57.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
 int main(){
-    int n= 7;
-    for(int i = 1 ; i <= n ; i++)
+    int n = 7;
+   for(int i = 1; i <= n; i++)
+   {
+    for(int j = 1 ; j <= n ; j++)
     {
-        for(int j = 1 ; j <= n ; j++)
-        {
-            if(i == 1 || i == n)
-            printf("1");
-            else if(i == n/2+1)
-            {
-                if(j == 1 || j == n/2+1 || j == n)
-                printf("*");
-                else
-                printf(" ");
-            }
-            else
-            {
-                if(j == 1 || j == n)
-                printf("*");
-                else
-                printf(" ");
-            }
-        }
-        printf("\n");
+        int top = i - 1;
+        int bot = n - i ;
+        int left = j - 1; 
+        int right = n - j ;
+
+        int min = top < bot ? top : bot ;
+        int min1 = left < right ? left : right;
+
+        int rel_min = min < min1 ? min : min1;
+        
+        printf("%d",rel_min+1);
     }
-    return 0;
+    printf("\n");
+   }
+   return 0;
 }
